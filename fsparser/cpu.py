101bfbed04c9ee3fbc76f5bf2c77f1313431500d
ford <cfd@yellow.local>
2024-08-10 23:51:56 -0700

    rm colon

--- fsparser/cpu.py
+++ fsparser/cpu.py
@@ -20,17 +20,17 @@ def get_proc_stat(fd, keys, output_method):
     cpu_keys = keys - {'ctxt', 'intr'}
     cpu_line = fd.readline().split()
     for key in cpu_keys:
-        output_method(f'perf.proc.stat.{key}:', cpu_line[cpu_idx_dict[key]])
+        output_method(f'perf.proc.stat.{key}', cpu_line[cpu_idx_dict[key]])
 
     for core_id in range(os.cpu_count()):
         core_line = fd.readline().split()
         for key in cpu_keys:
-            output_method(f'perf.proc.stat.cpu{core_id}.{key}:', core_line[cpu_idx_dict[key]])
+            output_method(f'perf.proc.stat.cpu{core_id}.{key}', core_line[cpu_idx_dict[key]])
 
     intr_line = fd.readline().split()
     if 'intr' in keys:
-        output_method(f'perf.proc.stat.intr:', intr_line[1])
+        output_method(f'perf.proc.stat.intr', intr_line[1])
 
     ctxt_line = fd.readline().split()
     if 'ctxt' in keys:
-        output_method(f'perf.proc.stat.ctxt:', ctxt_line[1])
+        output_method(f'perf.proc.stat.ctxt', ctxt_line[1])
